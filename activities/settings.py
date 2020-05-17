cca988b8f9011d4fa4c5519a44c9741ea48016c3
RashmiShah <ratnesh.choudhary@yashco.com>
2020-05-18 01:51:25 +0530

    changes in deployment of app

--- activities/settings.py
+++ activities/settings.py
@@ -145,3 +145,4 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 AUTH_USER_MODEL = 'activityapp.Member'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
